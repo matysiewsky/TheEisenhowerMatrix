acf0b85924b9297f9b82ed9e077b8274caec6ec0
Maciej M <matysiewskimaciej@gmail.com>
2021-12-02 21:34:48 +0100

    Implemented saving to csv

--- TheEisenhowerMatrix/DataManager.cs
+++ TheEisenhowerMatrix/DataManager.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using CsvHelper;
+
 namespace TheEisenhowerMatrix
 {
     public class DataManager
     {
+        public List<Item> ReadFromCSV()
+        {
 
+        }
+
+        public void SaveToCSV(Matrix matrixName, List<Item> itemsToSave)
+        {
+            using StreamWriter FileWriter = new($"data/{matrixName}");
+            using (CsvWriter CSVWriter = new(FileWriter, CultureInfo.CurrentCulture))
+            {
+                CSVWriter.WriteRecords(itemsToSave);
+            }
+        }
     }
 }
