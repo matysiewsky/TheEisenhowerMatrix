7179ee72ded7e997bdf1cefa15f40f0ca77108ef
Maciej M <matysiewskimaciej@gmail.com>
2021-12-03 22:23:19 +0100

    DataManager fully implemented

--- TheEisenhowerMatrix/DataManager.cs
+++ TheEisenhowerMatrix/DataManager.cs
@@ -1,25 +1,54 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using CsvHelper;
 
 namespace TheEisenhowerMatrix
 {
-    public class DataManager
+    public static class DataManager
     {
-        public List<Item> ReadFromCSV()
-        {
-
-        }
-
-        public void SaveToCSV(Matrix matrixName, List<Item> itemsToSave)
+        public static void SaveToCSV(Matrix matrixName, List<Item> itemsToSave)
         {
             using StreamWriter FileWriter = new($"data/{matrixName}");
             using (CsvWriter CSVWriter = new(FileWriter, CultureInfo.CurrentCulture))
             {
                 CSVWriter.WriteRecords(itemsToSave);
             }
+        }
+        public static List<Item> ReadFromCSV(string filename)
+        {
+            List<Item> records;
+
+            using StreamReader FileReader = new($"data/{filename}");
+            using (CsvReader CSVReader = new(FileReader, CultureInfo.InvariantCulture))
+            {
+                records = CSVReader.GetRecords<Item>().ToList();
+            }
+
+            return records;
+        }
+
+        public static string[] GetSavedData()
+        {
+            string[] savedData = Directory.GetFiles("data", "*.csv");
+
+            return savedData;
+        }
+
+        public static Matrix ImportUserData(string filename)
+        {
+            List<Item> itemsToImport = ReadFromCSV(filename);
+
+            Matrix importedMatrix = new(filename);
+
+            foreach (Item item in itemsToImport)
+            {
+                importedMatrix.AddItem(item);
+            }
+
+            return importedMatrix;
         }
     }
 }
