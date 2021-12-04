69e7792d2177968233f83d023f3169858767a715
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2021-12-05 00:19:12 +0100

    dummy part of code for tests - to be deleted

--- TheEisenhowerMatrix/Program.cs
+++ TheEisenhowerMatrix/Program.cs
@@ -6,6 +6,19 @@ namespace TheEisenhowerMatrix
     {
         static void Main(string[] args)
         {
+            // FOR TESTS:
+            Matrix matrix1 = new Matrix("matrix1");
+
+            matrix1.AddItem(new Item("learn to code", ItemType.Urgentimportant, new DateTime(2021, 12, 09)));
+            matrix1.AddItem(new Item("sleep", ItemType.Noturgentimportant, new DateTime(2021, 12, 05)));
+            matrix1.AddItem(new Item("watch tv", ItemType.Noturgentnotimportantitems, new DateTime(2021, 12, 07)));
+            matrix1.AddItem(new Item("read book", ItemType.Noturgentnotimportantitems, new DateTime(2021, 12, 22)));
+            matrix1.AddItem(new Item("shopping", ItemType.Noturgentnotimportantitems, new DateTime(2021, 12, 16)));
+
+            var dictionary = matrix1.CreateDictionaryOfItems();
+            MatrixManager.CreateAndDisplayMatrix(dictionary);
+
+
             Display.DisplayHelloMessage();
             Display.FreezeDisplay(4);
             // Display.ClearDisplay();
